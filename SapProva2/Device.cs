df67d8a5fad7d83c81c870fa8f0575907167f658
Luka Zorc <rawwer@gmail.com>
2015-10-29 11:16:49 +0100

    First commit

---
+++ SapProva2/Device.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Devices.Gpio;
+using Windows.Devices.Spi;
+using Windows.Devices.Enumeration;
+
+namespace SapProva2
+{
+    public class Device
+    {
+        /*RaspBerry Pi2  Parameters*/
+        private const string SpiControllerName = "SPI0";  /* For Raspberry Pi 2, use SPI0                             */
+        private const int SpiChipSelectLine = 0;       /* Line 0 maps to physical pin number 24 on the Rpi2        */
+
+        /*Uncomment if you are using mcp3208/3008 which is 12 bits output */
+        readonly byte[] _readBuffer = new byte[3]; /*this is defined to hold the output data*/
+        readonly byte[] _writeBuffer = new byte[3] { 0x06, 0x00, 0x00 };//00000110 00; // It is SPI port serial input pin, and is used to load channel configuration data into the device
+
+        /*Uncomment if you are using mcp3002*/
+        /* byte[] readBuffer = new byte[3]; /*this is defined to hold the output data*/
+        // byte[] writeBuffer = new byte[3] { 0x68, 0x00, 0x00 };//01101000 00; /* It is SPI port serial input pin, and is used to load channel configuration data into the device*/
+
+        private SpiDevice _spiDisplay;
+        private const int LedPin = 5;
+        public GpioPin Pin { get; set; }
+        public GpioPinValue PinValue { get; set; }
+
+        public void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                Pin = null;
+                Debug.WriteLine("There is no GPIO controller on this device.");
+                return;
+            }
+
+            Pin = gpio.OpenPin(LedPin);
+            PinValue = GpioPinValue.High;
+            Pin.Write(PinValue);
+            Pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            Debug.WriteLine("GPIO pin initialized correctly.");
+        }
+
+        public async void InitSPI()
+        {
+            try
+            {
+                var settings = new SpiConnectionSettings(SpiChipSelectLine);
+                settings.ClockFrequency = 500000; // 10000000;
+                settings.Mode = SpiMode.Mode0; //Mode3;
+
+                var spiAqs = SpiDevice.GetDeviceSelector(SpiControllerName);
+                var deviceInfo = await DeviceInformation.FindAllAsync(spiAqs);
+                _spiDisplay = await SpiDevice.FromIdAsync(deviceInfo[0].Id, settings);
+            }
+
+            /* If initialization fails, display the exception and stop running */
+            catch (Exception ex)
+            {
+                throw new Exception("SPI Initialization Failed", ex);
+            }
+        }
+
+        public double Result()
+        {
+            _spiDisplay.TransferFullDuplex(_writeBuffer, _readBuffer);
+            var temp = ConvertToDouble(_readBuffer);
+            return temp;
+        }
+
+        private static double ConvertToDouble(byte[] data)
+        {
+            /*Uncomment if you are using mcp3208/3008 which is 12 bits output */
+            var result = data[1] & 0x0F;
+            result <<= 8;
+            result += data[2];
+            var millivolts = Convert.ToDouble(result) * (3.4 / 4095); // (VOLTS / 4095) Volte prebrat z multimetrom.
+            var tempC = (millivolts - 0.5) * 100;
+            return tempC;
+        }
+    }
+}
---
+++ SapProva2/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+using Windows.Devices.Gpio;
+using Newtonsoft.Json;
+
+
+namespace SapProva2
+{
+    public sealed partial class MainPage : Page
+    {
+        Device device = new Device();
+        Uri uri = new Uri(
+                    "https://iotmmsp1941683943trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/1764772f-f48c-43b8-8186-ff41348d7796");
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+           
+            device.InitGPIO();
+            device.InitSPI();
+            if (device.Pin != null)
+            {
+                var timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromMilliseconds(5000);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {  
+            Send();
+            Poll();
+        }
+
+        
+
+        private async void Send()
+        {
+            var temp = device.Result();
+
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + "bdb31fd4655dcf1ef85823d59ba92fc");
+
+            //Find unix timestamp (seconds since 01/01/1970)
+            var ticks = DateTime.UtcNow.Ticks - DateTime.Parse("01/01/1970 00:00:00").Ticks;
+            ticks /= 10000000; //Convert windows ticks to seconds
+
+            var postData =
+                "{\"mode\":\"async\",\"messageType\":\"0c6c466ffd27fb36d0ce\",\"messages\":[{\"sensor\": \"1\",\"value\":" +temp+ ",\"timestamp\":" +ticks+ "}]}";
+            var content = new HttpStringContent(postData, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json");
+            var postResult = await httpClient.PostAsync(uri, content);
+
+            try
+            {
+                if (postResult.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("Message Sent: {0}", content);
+                }
+                else
+                {
+                    Debug.WriteLine("Failed sending message: {0}", postResult.ReasonPhrase);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception when sending message:" + e.Message);
+            }
+
+        }
+
+        private async void Poll()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + "bdb31fd4655dcf1ef85823d59ba92fc");
+            var response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var result = responseBody.Equals("[]", StringComparison.Ordinal);
+            if (result != true)
+            {
+                var jsonObject = JsonConvert.DeserializeObject<List<RootObject>>(responseBody); 
+                
+                foreach (var post in jsonObject)
+                {
+                    Debug.WriteLine(post.Messages[0].Operand);
+                    var ledValue = post.Messages[0].Operand;
+                    if (ledValue == 0)
+                    {
+                        device.PinValue = GpioPinValue.Low;
+                        device.Pin.Write(device.PinValue);
+                    }
+                    else
+                    {
+                        device.PinValue = GpioPinValue.High;
+                        device.Pin.Write(device.PinValue);
+                    }
+                }
+            }
+        }
+
+        public class RootObject
+        {
+            public string MessageType { get; set; }
+            public string Sender { get; set; }
+            public List<RootObject2> Messages { get; set; }
+        }
+
+        public class RootObject2
+        {
+            public string Opcode { get; set; }
+            public int Operand { get; set; }
+        }
+    }
+
+    
+}
